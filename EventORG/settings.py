5811b1d0a7be5b2979d57ada53b8b27e4cf2ec9d
pritam <pritam.pk36128@gmail.com>
2023-07-02 16:16:45 +0530

    config for vercel-4

--- EventORG/settings.py
+++ EventORG/settings.py
@@ -123,6 +123,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
